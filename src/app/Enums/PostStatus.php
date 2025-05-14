0279e3a3bfb1218f47de261e5461e8b28fc46bd4
miyahara <kenmiya.sns@gmail.com>
2025-05-15 00:33:14 +0900

    [refactor]投稿ステータスの定数名を修正し一貫性を持たせた

--- src/app/Enums/PostStatus.php
+++ src/app/Enums/PostStatus.php
@@ -6,7 +6,7 @@ enum PostStatus: int
 {
     case REGISTERED = 1;
     case IS_CHECKED = 10;
-    case NICE_JOKER = 11;
-    case NOT_JOKER  = 12;
+    case NICE_JOKE  = 11;
+    case NOT_JOKE   = 12;
     case DELETED    = 99;
 }
